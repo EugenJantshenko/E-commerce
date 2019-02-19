f7236001ff1adc92153176cd980a9842df78c907
Eugen Jantshenko <kryoengineer@gmail.com>
2019-02-19 22:59:13 +0300

    19.02

---
+++ src/main/java/com/Test2.java
@@ -0,0 +1,42 @@
+package com;
+
+import java.lang.Math.*;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.io.*;
+
+import static java.lang.Math.incrementExact;
+import static java.lang.Math.max;
+
+interface I {
+    public static final int Ea = 5;
+}
+
+public class Test2 implements I {
+/////////////////
+
+    public static void main(String[] args) {
+//////////////////
+
+        int a1 = 1000;
+        int a2 = 1000;
+//
+     //   a1.valu
+//        Double d1 = new Double(10d);
+//        Double d2 = new Double(10d);
+
+//        int a1 = 1000;
+//        int a2 = 1000;
+
+        Double d1 = 10d;
+        Double d2 = 10d;
+        System.out.println(a1 == a2);
+        System.out.println(d1 == d2);
+
+/////////////////
+    }
+}
+
+
+
