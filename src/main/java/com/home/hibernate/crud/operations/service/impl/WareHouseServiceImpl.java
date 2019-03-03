5ad80a792799118fb971c101bb898ffb7a1ae397
Eugen Jantshenko <kryoengineer@gmail.com>
2019-03-03 19:43:12 +0300

    03.03

--- src/main/java/com/home/hibernate/crud/operations/service/impl/WareHouseServiceImpl.java
+++ src/main/java/com/home/hibernate/crud/operations/service/impl/WareHouseServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Date;
+//import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
